5804c7f722d6e928c330e67b641eb165fab44e76
Your Name <you@example.com>
2020-09-14 18:28:09 +0800

    增加一些函数api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,61 @@
 <template>
   <div class="home">
     {{ state.msg }}
+    {{ plus }}
     <img alt="Vue logo" src="../assets/logo.png" />
+    <div>random:{{ random() }}</div>
+    <button type="button" @click="handleClick">button</button>
   </div>
 </template>
 <script lang="ts">
-import { reactive } from "vue"
+import { reactive, ref, computed, watch, watchEffect } from "vue"
 export default {
   setup() {
     const state = reactive({
       msg: "msg reactive"
     })
-
-    const random = function(): void {
-      state.msg = Math.random()
+    const count = ref(1)
+    const count2 = ref(12)
+    const methods = {
+      random(): number {
+        //state.msg = "123456"
+        return Math.random() * 10
+      },
+      handleClick(): void {
+        state.msg = "hasClick"
+        count.value++
+      }
     }
 
+    const stop = watchEffect(() => console.log(count2.value)) //立即执行传入的一个函数，并响应式追踪其依赖，并在其依赖变更时重新运行该函数。
+
+    count2.value++
+    const plus = computed({
+      get: () => count.value + 1,
+      set: val => {
+        count.value = val - 1
+      }
+    })
+    // watch(       //侦听单个响应式数据
+    //   () => state.msg,
+    //   (msg, prevMsg) => {
+    //     //当state.msg更新，会触发此函数
+    //     console.log(msg)
+    //     console.log(prevMsg)
+    //   }
+    // )
+    watch([() => state.msg, count], ([msg, count], [prevMsg, prevCount]) => {
+      //侦听多个响应式数据
+      // 当 count.value 或者 count2.value 更新，会触发此回调函数
+      console.log(msg)
+      console.log(count)
+      console.log(prevMsg)
+      console.log(prevCount)
+    })
     return {
-      random,
-      state
+      state,
+      plus,
+      ...methods
     }
   }
 }
